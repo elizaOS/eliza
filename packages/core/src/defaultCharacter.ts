bc2eca33abb8c5ba53bc305cd5c93ff005daeda8
0xNerd <thejoker3077@gmail.com>
2025-01-07 10:33:18 -0500

    echo

--- packages/core/src/defaultCharacter.ts
+++ packages/core/src/defaultCharacter.ts
@@ -1,9 +1,10 @@
 import { Character, ModelProviderName, Clients } from "./types.ts";
+import { echoChamberPlugin } from "@ai16z/plugin-echochambers";
 
 export const defaultCharacter: Character = {
     name: "DEGEN8BALL",
     username: "degen8ball",
-    plugins: [],
+    plugins: [echoChamberPlugin],
     clients: [
         Clients.TWITTER,
         //Clients.TELEGRAM,
