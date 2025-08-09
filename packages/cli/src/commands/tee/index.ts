45c222becea52fd39e9efa3d1fcd14a75b7691e4
Christopher Trimboli <cjft@trimboli.io>
2025-08-09 10:29:11 -0600

    clean slate

--- packages/cli/src/commands/tee/index.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from 'commander';
-import { phalaCliCommand } from './phala-wrapper';
-
-export const teeCommand = new Command('tee')
-  .description('Manage TEE deployments')
-  // Add TEE Vendor Commands
-  .addCommand(phalaCliCommand);
