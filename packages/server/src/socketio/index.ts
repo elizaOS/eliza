4791ce3d9600792fc5c2dc4b42a14bacac9a203f
standujar <s.andujar@proton.me>
2025-12-11 18:57:28 +0100

    fix(tests): enhance logger-browser-node test to specify log level for browser type

--- packages/server/src/socketio/index.ts
+++ packages/server/src/socketio/index.ts
@@ -203,10 +203,10 @@ export class SocketIORouter {
       const { type, payload } = data;
 
       switch (type) {
-        case String(SOCKET_MESSAGE_TYPE.ROOM_JOINING):
+        case SOCKET_MESSAGE_TYPE.ROOM_JOINING:
           this.handleChannelJoining(socket, payload);
           break;
-        case String(SOCKET_MESSAGE_TYPE.SEND_MESSAGE):
+        case SOCKET_MESSAGE_TYPE.SEND_MESSAGE:
           this.handleMessageSubmission(socket, payload);
           break;
         default:
