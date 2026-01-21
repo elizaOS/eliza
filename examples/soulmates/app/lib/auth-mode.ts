05d4ca11d769db8c7f54a722ee24b2ce2b951543
Shaw <shawmakesmagic@gmail.com>
2026-01-21 17:10:44 -0500

    chore: add plugins to lerna, add .npmignore to all plugins, bump to 2.0.0-alpha.1

--- examples/soulmates/app/lib/auth-mode.ts
+++
@@ -1,7 +0,0 @@
-import { readEnv } from "@/lib/env";
-
-export const DEV_SESSION_COOKIE = "soulmates-dev-session";
-
-export function isAuthEnabled(): boolean {
-  return readEnv("NEXTAUTH_SECRET") !== null;
-}
