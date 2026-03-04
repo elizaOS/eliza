f351a116f50e07c78af65eb0d091c2296212211e
Odilitime <janesmith@airmail.cc>
2026-03-04 04:17:07 +0000

    stores: 💡 Minor: Type Safety (plugin.store.ts)
    Iteration 13
    
    prr-fix:ic-3995138905-2

--- stores/plugin.store.ts
+++ stores/plugin.store.ts
@@ -1,1 +1,29 @@
-COMPLETE FILE CONTENTS WITH FIXES
+import { Plugin } from './types';
+
+export const pluginStore = {
+    plugins: [] as Plugin[],
+    
+    addPlugin(plugin: unknown) {
+        if (this.isValidPlugin(plugin)) {
+            this.plugins.push(plugin);
+        }
+    },
+
+    isValidPlugin(plugin: unknown): plugin is Plugin {
+        return typeof plugin === 'object' && plugin !== null && 'id' in plugin;
+    },
+
+    getPluginById(id: string): Plugin | undefined {
+        return this.plugins.find((plugin: Plugin) => plugin.id === id);
+    },
+
+    removePluginById(id: string): boolean {
+        const index = this.plugins.findIndex((plugin: Plugin) => plugin.id === id);
+        if (index > -1) {
+            this.plugins.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+};
+
