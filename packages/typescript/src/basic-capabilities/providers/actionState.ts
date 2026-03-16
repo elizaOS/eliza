6dc9e9ae51bd18aee56af21794d54e5bb1e8d6e0
Odilitime <janesmith@airmail.cc>
2026-03-16 21:12:23 +0000

    packages: Potential issue | 🟠 Major (actionState.ts)
    Iteration 5
    
    prr-fix:prrc_kwdomt5cis6td3o7

--- packages/typescript/src/basic-capabilities/providers/actionState.ts
+++ packages/typescript/src/basic-capabilities/providers/actionState.ts
@@ -99,7 +99,6 @@ export const actionStateProvider: Provider = {
           })() +
           80,
         ACTION_RESULTS_TARGET_CHARS,
-      // Note: includes result.data length to accommodate potential future use of rendered data fields
       );
 
       const formattedResults = selectedResults
