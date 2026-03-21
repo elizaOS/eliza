2e7157f403b62a28859749cb1f472a2e0af0e64a
Shaw <shawmakesmagic@gmail.com>
2026-03-20 17:17:57 -0700

    refactor out app core, eliza agent

--- packages/app-core/test/test-types.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Re-export react-test-renderer types for use in tests.
- * Use: import type { ReactTestRenderer, ReactTestInstance } from "../test/test-types";
- */
-export type { ReactTestRenderer, ReactTestInstance } from "react-test-renderer";
