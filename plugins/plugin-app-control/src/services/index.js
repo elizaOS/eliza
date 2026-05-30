2e31f817c3cf0959efaea39235373c6a5754fd71
Shaw <shaw@elizaos.ai>
2026-05-30 02:16:16 -0400

    chore(app-control): refresh generated js outputs

--- plugins/plugin-app-control/src/services/index.js
+++ plugins/plugin-app-control/src/services/index.js
@@ -2,5 +2,5 @@
  * @module plugin-app-control/services
  * @description Barrel export for plugin services.
  */
-export { AppVerificationService } from "./app-verification.js";
+export { AppVerificationService, } from "./app-verification.js";
 //# sourceMappingURL=index.js.map
