1d776d920d44b0120360c6015ab857c3d54cab4d
Odilitime <janesmith@airmail.cc>
2026-06-27 20:32:41 -0700

    Make drawer handles visibly interactive

--- packages/ui/src/cloud-ui/components/drawer.tsx
+++ packages/ui/src/cloud-ui/components/drawer.tsx
@@ -69,7 +69,18 @@ function DrawerContent({
         )}
         {...props}
       >
-        <div className="mx-auto mt-4 hidden h-1.5 w-[100px] shrink-0 bg-white/15 group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
+        <DrawerClose asChild>
+          <button
+            type="button"
+            aria-label="Close drawer"
+            className="group mx-auto mb-2 mt-2 hidden h-8 w-32 shrink-0 cursor-pointer items-center justify-center rounded-full transition-colors hover:bg-white/10 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/45 group-data-[vaul-drawer-direction=bottom]/drawer-content:flex"
+          >
+            <span
+              className="h-1.5 w-[100px] rounded-full bg-white/15 transition-all group-hover:w-[112px] group-hover:bg-white/35 group-focus-visible:w-[112px] group-focus-visible:bg-white/45"
+              aria-hidden
+            />
+          </button>
+        </DrawerClose>
         {children}
       </DrawerPrimitive.Content>
     </DrawerPortal>
