31a36962eaf3a5f95a4a51ac697927b3acc797b0
Stakemate369 <Stakemate@proton.me>
2026-07-19 21:22:04 -0600

    Add YieldSignal plugin: src/index.ts

---
+++ plugins/plugin-yieldsignal/src/index.ts
@@ -0,0 +1,13 @@
+import type { Plugin } from "@elizaos/core";
+import { getYieldSignalAction } from "./actions/get-yield-signal.js";
+
+export { getYieldSignalAction } from "./actions/get-yield-signal.js";
+export { fetchYieldSignal, type YieldSignalAsset } from "./client.js";
+
+export const yieldSignalPlugin: Plugin = {
+  name: "yieldsignal",
+  description: "Paid (x402) real-time USDC/WETH lending yield signal on Base, signed and EAS-attested on-chain.",
+  actions: [getYieldSignalAction],
+};
+
+export default yieldSignalPlugin;
