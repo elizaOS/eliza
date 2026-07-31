bd1c3c34736b2a2d5394c601c9b94df07f0a0e86
NubsCarson <nubs@nubs.site>
2026-07-31 14:54:47 -0400

    fix(core): suppress deferred action model streams

--- packages/core/src/runtime.ts
+++ packages/core/src/runtime.ts
@@ -146,7 +146,6 @@ import {
 	getStreamingContext,
 	runInsideModelStreamChunkDelivery,
 	runWithStreamingContext,
-	runWithSuppressedModelStream,
 } from "./streaming-context";
 import {
 	getTrajectoryContext,
@@ -4052,15 +4051,13 @@ export class AgentRuntime implements IAgentRuntime {
 							runWithActionRoutingContext(
 								{ actionName: action.name, modelClass: action.modelClass },
 								() =>
-									runWithSuppressedModelStream(() =>
-										action.handler(
-											this,
-											message,
-											composedState,
-											{ mode },
-											actionCallback,
-											options?.responses,
-										),
+									action.handler(
+										this,
+										message,
+										composedState,
+										{ mode },
+										actionCallback,
+										options?.responses,
 									),
 							),
 					});
