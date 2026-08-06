fd7f25dba712fb2ee27fb0e6f1713760d9508ba3
solizardking <beetsbyj@gmail.com>
2026-08-06 18:13:56 -0400

    adfsad

--- my-project/apps/app/.vite/deps/@capacitor_app.js
+++
@@ -1,5 +0,0 @@
-import { registerPlugin } from "./@capacitor_core.js";
-//#region ../../node_modules/@capacitor/app/dist/esm/index.js
-var App = registerPlugin("App", { web: () => import("./web-HVLy8TPR.js").then((m) => new m.AppWeb()) });
-//#endregion
-export { App };
